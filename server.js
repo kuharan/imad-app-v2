3620f58c9e07d04484da24cabef36d02232dd629
KUHARAN <kuharan.bhowmik@gmail.com>
2017-02-18 23:11:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,19 +104,11 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName',function(req,res){
+    var articleName=req.params.articleName;
     res.send(createTemplate(articles[articleName]));
-
 });
 
-app.get('/:articleName',function(req,res){
-    res.send(createTemplate(articles[articleName]));
 
-});
-
-app.get('/:articleName',function(req,res){
-    res.send(createTemplate(articles[articleName]));
-
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
