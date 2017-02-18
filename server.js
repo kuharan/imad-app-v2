ccfa10f7e2249f0042cfa55baf995573db2c5f14
KUHARAN <kuharan.bhowmik@gmail.com>
2017-02-18 22:48:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 var articles = {
-    articleOne : {
+    articleone : {
     title: 'Raees | Kuharan Bhowmik',
     heading: 'Raees (film)',
     date: 'Feb 18,2017',
