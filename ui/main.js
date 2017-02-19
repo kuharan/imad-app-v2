f89c626bfb7134d27865fb9f51a5554ea8dfe071
KUHARAN <kuharan.bhowmik@gmail.com>
2017-02-19 21:45:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,3 +8,15 @@ img.onclick=function(){
     var Linterval=setInterval(moveLeft,50);
     
 };
+
+var boxWidth = 57, delta = 2;
+setInterval(function(){
+  var left = parseInt(img.style.left);
+  if(left >= parseInt(viewDim.width - boxWidth)){
+    delta = -2;
+  }
+  if (left <= 0) {
+    delta = 2;
+  }
+  img.style.left = left + delta + 'px';
+}, 20);
