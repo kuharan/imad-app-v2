4274a989e425c9a80d0d746d99d10e1e00067b17
KUHARAN <kuharan.bhowmik@gmail.com>
2017-02-23 20:58:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -121,7 +121,7 @@ app.get('/:articleName',function(req,res){
 var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
    
-    pool.query('select * from tag',fucntion(err,result){
+pool.query('select * from tag',function(err,result){
         if (err){
             res.status(500).send(err.toString());
         }else{
