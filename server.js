6ae35685d55fbb9b7e6cca88444e76a6b51bb700
KUHARAN <kuharan.bhowmik@gmail.com>
2017-03-11 22:26:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -126,7 +126,7 @@ app.get('/:test-db',function (req,res) {
     });
 });
 
-app.get('/:articleName',function(req,res){
+app.get('/articles/:articleName',function(req,res){
     var articleName=req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
