79b81006a4fbdc008a4130341571a3c557f1276b
KUHARAN <kuharan.bhowmik@gmail.com>
2017-03-12 16:35:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,14 +5,7 @@ submit.onclick = function(){
     request.onreadystatechange = function(){
         if(request.readyState === XMLHttpRequest.DONE){
             if (request.status === 200){
-                var names=request.responseText;
-                names=JSON.parse(names);
-                var list='';
-                for (var i=0 ; i<names.length ; i++){
-                    list+='<li>' + names[i] + '</li>';
-                }
-                var ul=document.getElementById('namelist');
-                ul.innerHTML = list;
+                Console.log('User logged in')
                 
             }
         }
