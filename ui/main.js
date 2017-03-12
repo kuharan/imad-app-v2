18a7b62dea746c698320f9183dba57c1b74a9906
KUHARAN <kuharan.bhowmik@gmail.com>
2017-03-12 19:20:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,139 +1,254 @@
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+var Pool = require('pg').Pool;
+var crypto=require('crypto');
+var bodyParser=require('body-parser');
+var session=require('express-session');
 
-function loadLoginForm () {
-    var loginHtml = `
-        <h3>Login/Register to unlock awesome features</h3>
-        <input type="text" id="username" placeholder="username" />
-        <input type="password" id="password" />
-        <br/><br/>
-        <input type="submit" id="login_btn" value="Login" />
-        <input type="submit" id="register_btn" value="Register" />
-        `;
-    document.getElementById('login_area').innerHTML = loginHtml;
-    
-    // Submit username/password to login
-    var submit = document.getElementById('login_btn');
-    submit.onclick = function () {
-        // Create a request object
-        var request = new XMLHttpRequest();
-        
-        // Capture the response and store it in a variable
-        request.onreadystatechange = function () {
-          if (request.readyState === XMLHttpRequest.DONE) {
-              // Take some action
-              if (request.status === 200) {
-                  submit.value = 'Sucess!';
-              } else if (request.status === 403) {
-                  submit.value = 'Invalid credentials. Try again?';
-              } else if (request.status === 500) {
-                  alert('Something went wrong on the server');
-                  submit.value = 'Login';
-              } else {
-                  alert('Something went wrong on the server');
-                  submit.value = 'Login';
-              }
-              loadLogin();
-          }  
-          // Not done yet
-        };
-        
-        // Make the request
-        var username = document.getElementById('username').value;
-        var password = document.getElementById('password').value;
-        console.log(username);
-        console.log(password);
-        request.open('POST', '/login', true);
-        request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify({username: username, password: password}));  
-        submit.value = 'Logging in...';
-        
-    };
-    
-    var register = document.getElementById('register_btn');
-    register.onclick = function () {
-        // Create a request object
-        var request = new XMLHttpRequest();
-        
-        // Capture the response and store it in a variable
-        request.onreadystatechange = function () {
-          if (request.readyState === XMLHttpRequest.DONE) {
-              // Take some action
-              if (request.status === 200) {
-                  alert('User created successfully');
-                  register.value = 'Registered!';
-              } else {
-                  alert('Could not register the user');
-                  register.value = 'Register';
-              }
-          }
-        };
-        
-        // Make the request
-        var username = document.getElementById('username').value;
-        var password = document.getElementById('password').value;
-        console.log(username);
-        console.log(password);
-        request.open('POST', '/create-user', true);
-        request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify({username: username, password: password}));  
-        register.value = 'Registering...';
-    
-    };
-}
+var config = {
+  host: 'db.imad.hasura-app.io',
+  user: 'kuharan',
+  password: process.env.DB_PASSWORD,
+  database: 'kuharan',
+  port:'5432'
+};
 
-/*function loadLoggedInUser (username) {
-    var loginArea = document.getElementById('login_area');
-    loginArea.innerHTML = `
-        <h3> Hi <i>${username}</i></h3>
-        <a href="/logout">Logout</a>
+var app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(session({
+    secret: 'someRandomSecretValue',
+    cookie: {maxAge:1000*60*60*24*30}
+}));
+
+
+
+
+/*var articles = {
+    'article-one' : {
+    title: 'Raees | Kuharan Bhowmik',
+    heading: 'Raees (film)',
+    date: 'Feb 18,2017',
+    content:
+            `<p>
+               Raees is a 2017 Indian action crime thriller film directed by Rahul Dholakia and produced by Gauri Khan,
+                Ritesh Sidhwani and Farhan Akhtar under their banners Red Chillies Entertainment and Excel Entertainment.
+                It stars Shah Rukh Khan, Mahira Khan and Nawazuddin Siddiqui.
+            </p>
+            <p>
+                Raees was said to be based on criminal Abdul Latif's life but, the filmmakers denied this, 
+                saying "The story of the film is a pure work of fiction, not based on any person; living or
+                dead." The film was released on 25 January 2017.
+            </p>
+            <p>
+                Raees, from Fatehpur, Gujarat gets involved in illegal liquor trade at a very young age. Along with Sadiq, Raees works for a notorious gangster Jairaj, smuggles alcohol illegally by bribing the police. Though he is a smuggler, he lives by the philosophy, as taught by his mother that every occupation is good, and no religion is greater than any occupation till it does not cause any harm to anyone. 
+            </p>
+            <p>
+                He decides to part ways with Jairaj and start operating on his own. He meets Musabhai and Nawab in Mumbai, and with their help he starts his bootlegging business. Meanwhile, an honest police offer of the IPS cadre, J. A. Majmudar, wants to end this illegal liquor trade.
+            </p>`
+    },
+     
+    'article-two' : {
+        title:'Dangal | Kuharan Bhowmik',
+        heading:'Dangal (film)',
+        date:'Feb 19,2017',
+        content:`
+            <p>
+               Dangal (English: Wrestling competition) is a 2016 Indian Hindi-language biographical sports drama
+               film directed by Nitesh Tiwari. It stars Aamir Khan as Mahavir Singh Phogat, who taught wrestling
+               to his daughters Geeta Phogat and Babita Kumari.
+            </p>
+            <p>
+               The former is India's first female wrestler to win
+               at the 2010 Commonwealth Games, where she won the gold medal (55 kg). Her sister Babita Kumari won
+               the silver (51 kg). "Dangal" is the Hindi term for "a wrestling competition".
+            </p>`
+    },
+    
+    'article-three' : {
+        title:'Sultan | Kuharan Bhowmik',
+        heading:'Sultan (film)',
+        date:'Feb 20,2017',
+        content:`
+            <p>
+               Sultan is a 2016 Indian romantic sports-drama film directed by Ali Abbas Zafar.
+               Produced by Aditya Chopra under the Yash Raj Films banner, the film stars Salman
+               Khan as the title character opposite Anushka Sharma. 
+            </p>
+            <p>
+               The film focuses on Sultan Ali Khan,
+               a fictional wrestling champion from Haryana whose successful career creates a rift in his personal life.
+            </p>`
+    }
+
+    
+ };*/
+
+function createTemplate(data){
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+    
+    var htmlTemplate=`
+    <html>
+        <head>
+            <title> ${title} </title>
+            <meta name="viewport" content="width=device-width,initial-scale=1" />
+            <link href="ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div><a href="/">Home</a><br>
+            </div>
+            <div>${date.toDateString()}</div>
+            <div><p>Copyright wikipedia</p></div>
+            <hr/>
+            <h3> ${heading}</h3>
+            <hr/>
+            <div class="container">
+                <div>
+                  ${content}
+                </div>
+            </div>
+        </body>
+    </html>
     `;
-}*/
-
-function loadLogin () {
-    // Check if the user is already logged in
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            if (request.status === 200) {
-                loadLoggedInUser(this.responseText);
-            } else {
-                loadLoginForm();
-            }
-        }
-    };
-    
-    request.open('GET', '/check-login', true);
-    request.send(null);
+    return htmlTemplate;
 }
 
-function loadArticles () {
-        // Check if the user is already logged in
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            var articles = document.getElementById('articles');
-            if (request.status === 200) {
-                var content = '<ul>';
-                var articleData = JSON.parse(this.responseText);
-                for (var i=0; i< articleData.length; i++) {
-                    content += `<li>
-                    <a href="/articles/${articleData[i].title}">${articleData[i].heading}</a>
-                    (${articleData[i].date.split('T')[0]})</li>`;
+
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+function hash(input, salt){
+    var hashed=crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return ["pbkdf2Sync", "10000", salt, hashed.toString('hex')].join('$');
+    
+}
+
+
+app.get('/hash/:input', function (req, res) {
+    
+    var hashedString=hash(req.params.input, 'this-is-some-random-string');
+    res.send(hashedString);
+
+});
+
+app.post('/create-user', function(req, res) {
+    //JSON
+    var username=req.body.username;
+    var password=req.body.password;
+    
+    var salt=crypto.randomBytes(128).toString('hex');
+    var dbString=hash(password, salt);
+    pool.query('INSERT INTO "user" (username,password) VALUES ($1, $2)', [username, dbString], function(err, result){
+        if (err){
+            res.status(500).send(err.toString());
+        }else{
+            res.send('User Successfully Created' + username);
+        } 
+    });
+});
+
+app.post('/login', function(req, res) {
+    //JSON
+    var username=req.body.username;
+    var password=req.body.password;
+    
+   
+    
+    pool.query('SELECT * FROM "user" WHERE username = $1', [username], function(err, result){
+        if (err){
+            res.status(500).send(err.toString());
+        }else{
+            if(result.rows.length===0){
+                res.send(403).send('username/password invalid');
+            }else{
+                //match the password
+                var dbString=result.rows[0].password;
+                var salt=dbString.split('$')[2];
+                var hashedPassword=hash(password,salt);
+                if (hashedPassword===dbString){
+                    //set a session
+                    req.session.auth = {userId:result.rows[0].id};
+                    
+                    res.send('Credentials Correct');
+                    
+                    
+                    
+                    
+                }else{
+                     res.send(400).send('username/password invalid');
                 }
-                content += "</ul>"
-                articles.innerHTML = content;
-            } else {
-                articles.innerHTML('Oops! Could not load all articles!')
+            }
+            
+        } 
+    });
+});
+
+
+app.get('/check-login',function(req,res){
+   if(req.session && req.session.auth && req.session.auth.userId){
+       res.send('you are logged in: ' + req.session.auth.userId.toString());
+   }else{
+       res.send('you are not logged in');
+   }
+
+});
+
+app.get('/logout',function(req,res){
+    delete req.session.auth;
+    res.send('You are Logged in');
+});
+
+var pool = new Pool(config);
+
+app.get('/:test-db',function (req,res) {
+   
+    pool.query('SELECT * FROM article', function(err,result){
+        if (err){
+            res.status(500).send(err.toString());
+        }else{
+            res.send(JSON.stringify(result.rows));
+        }
+    });
+});
+
+app.get('/articles/:articleName', function(req,res){
+    
+pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        }else{
+            if(result.rows.length===0){
+                res.status(404).send('Article not found');
+            }else{
+                var articleData=result.rows[0];
+                res.send(createTemplate(articleData));
             }
         }
-    };
+    });
     
-    request.open('GET', '/get-articles', true);
-    request.send(null);
-}
+});
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
 
 
-// The first thing to do is to check if the user is logged in!
-loadLogin();
-
-// Now this is something that we could have directly done on the server-side using templating too!
-loadArticles();
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+  
+});
