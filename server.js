5861d361d954ca2b0132a4ce92eceb20c25b9a65
KUHARAN <kuharan.bhowmik@gmail.com>
2017-03-12 15:50:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -151,12 +151,15 @@ app.post('/login', function(req, res) {
     var username=req.body.username;
     var password=req.body.password;
     
-    var salt=crypto.randomBytes(128).toString('hex');
-    var dbString=hash(password, salt);
-    pool.query('INSERT INTO "user" (username,password) VALUES ($1, $2)', [username, dbString], function(err, result){
+   
+    
+    pool.query('SELECT * FROM "user" username = $1', [username], function(err, result){
         if (err){
             res.status(500).send(err.toString());
         }else{
+            if(result.rows.length===0){
+                res.send(400).send('username/password invalid');
+            }
             res.send('User Successfully Created' + username);
         } 
     });
