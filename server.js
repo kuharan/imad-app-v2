cf496b8de70c105d32623f11f8b0891f1de73461
KUHARAN <kuharan.bhowmik@gmail.com>
2017-03-12 17:15:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -211,6 +211,9 @@ pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], f
     
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
